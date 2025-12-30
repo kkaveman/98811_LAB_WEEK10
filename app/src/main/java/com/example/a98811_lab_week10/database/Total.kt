4aec5d9f8e5395a607201662d21245a4753187b7
kkaveman <kevinanatta1@gmail.com>
2025-12-30 21:33:29 +0700

    Commit No. 3: Integrate Room Database for Data Persistence

---
+++ app/src/main/java/com/example/a98811_lab_week10/database/Total.kt
@@ -0,0 +1,20 @@
+package com.example.a98811_lab_week10.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "total")
+// Every variables declared inside this class counts
+// as declaring a new column inside the table
+data class Total(
+// @PrimaryKey sets a column into a primary key
+    @PrimaryKey(autoGenerate = true)
+// @ColumnInfo sets the name of the column
+    @ColumnInfo(name = "id")
+// The variable name can be different from the column name
+    val id: Long = 0,
+// Here we set another column to store the total value
+    @ColumnInfo(name = "total")
+    val total: Int = 0,
+)
